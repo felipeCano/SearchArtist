b9fbb6787bb8e12f7e82a040c1cb0a3c58f0f7ee
Luis Felipe Cano A <fcano@carropago.com>
2026-03-05 00:43:37 -0500

    [viewModel]-> Create ViewModel and ViewModelFactory

---
+++ app/src/main/java/com/search/artist/presentation/viewmodel/SearchArtistViewModel.kt
@@ -0,0 +1,71 @@
+package com.search.artist.presentation.viewmodel
+
+import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.search.artist.data.model.searchArtist.SearchArtistResponse
+import com.search.artist.data.util.Resource
+import com.search.artist.domain.usecase.SearchArtistUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class SearchArtistViewModel(
+    private val searchArtistUseCase: SearchArtistUseCase,
+    private val app: Application
+) : AndroidViewModel(app) {
+    private val _searchArtist = MutableStateFlow<Resource<SearchArtistResponse>>(Resource.Loading())
+    val searchArtist = _searchArtist.asStateFlow()
+
+    fun searchArtist(query: String, page: Int, perPage: Int) {
+        viewModelScope.launch {
+            _searchArtist.value = Resource.Loading()
+            try {
+
+
+                if (isInternetAvailable(app)) {
+                    val apiResult = searchArtistUseCase.execute(query, page, perPage)
+                    _searchArtist.value = apiResult
+                } else {
+                    _searchArtist.value = Resource.Error("No internet connection")
+                }
+            }catch (e:Exception){
+                _searchArtist.value = Resource.Error(e.message.toString())
+            }
+        }
+    }
+
+    @Suppress("DEPRECATION")
+    fun isInternetAvailable(context: Context): Boolean {
+        var result = false
+        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager?
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            cm?.run {
+                cm.getNetworkCapabilities(cm.activeNetwork)?.run {
+                    result = when {
+                        hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                        hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                        hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                        else -> false
+                    }
+                }
+            }
+        } else {
+            cm?.run {
+                cm.activeNetworkInfo?.run {
+                    if (type == ConnectivityManager.TYPE_WIFI) {
+                        result = true
+                    } else if (type == ConnectivityManager.TYPE_MOBILE) {
+                        result = true
+                    }
+                }
+            }
+        }
+        return result
+    }
+}
